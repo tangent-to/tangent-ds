18d65ebeb363734a2058e235c5c785b50cf44ee4
Essi Parent <9ivko1ozf@relay.firefox.com>
2025-11-20 12:36:23 -0500

    wip

--- src/ml/index.js
+++ src/ml/index.js
@@ -2,56 +2,68 @@
  * Machine Learning module exports
  */
 
-import * as kmeans from './kmeans.js';
-import * as dbscan from './dbscan.js';
-import * as hdbscan from './hdbscan.js';
-import * as hdbscan_fast from './hdbscan_fast.js';
-import * as hca from './hca.js';
-import * as polynomial from './polynomial.js';
-import * as silhouette from './silhouette.js';
-import * as mlp from './mlp.js';
-import * as validation from './validation.js';
-import * as preprocessing from './preprocessing.js';
-import * as metrics from './metrics.js';
-import * as distances from './distances.js';
-import * as criteria from './criteria.js';
-import * as utils from './utils.js';
-import * as interpret from './interpret.js';
-import * as loss from './loss.js';
-import * as train from './train.js';
-import * as tuning from './tuning.js';
-import { GridSearchCV, Pipeline } from './pipeline.js';
-import { Recipe, recipe } from './recipe.js';
-import { BranchPipeline } from '../pipeline/BranchPipeline.js';
+import * as kmeans from "./kmeans.js";
+import * as dbscan from "./dbscan.js";
+import * as hca from "./hca.js";
+import * as polynomial from "./polynomial.js";
+import * as silhouette from "./silhouette.js";
+import * as mlp from "./mlp.js";
+import * as validation from "./validation.js";
+import * as preprocessing from "./preprocessing.js";
+import * as metrics from "./metrics.js";
+import * as distances from "./distances.js";
+import * as criteria from "./criteria.js";
+import * as utils from "./utils.js";
+import * as interpret from "./interpret.js";
+import * as loss from "./loss.js";
+import * as train from "./train.js";
+import * as tuning from "./tuning.js";
+import { GridSearchCV, Pipeline } from "./pipeline.js";
+import { Recipe, recipe } from "./recipe.js";
+import { BranchPipeline } from "../pipeline/BranchPipeline.js";
 
 // Imputation (missing data handling)
-import { SimpleImputer, KNNImputer, IterativeImputer, simpleImpute, knnImpute, iterativeImpute } from './impute.js';
+import {
+  SimpleImputer,
+  KNNImputer,
+  IterativeImputer,
+  simpleImpute,
+  knnImpute,
+  iterativeImpute,
+} from "./impute.js";
 
 // Outlier detection
-import { IsolationForest, LocalOutlierFactor, MahalanobisDistance, isolationForest, localOutlierFactor, mahalanobisDistance } from './outliers.js';
+import {
+  IsolationForest,
+  LocalOutlierFactor,
+  MahalanobisDistance,
+  isolationForest,
+  localOutlierFactor,
+  mahalanobisDistance,
+} from "./outliers.js";
 
 // Class-based estimators (scikit-like)
-import { KMeans } from './estimators/KMeans.js';
-import { DBSCAN } from './estimators/DBSCAN.js';
-import { HDBSCAN } from './estimators/HDBSCAN.js';
-import { HDBSCANFast } from './estimators/HDBSCANFast.js';
-import { HCA } from './estimators/HCA.js';
-import { ConsensusCluster } from '../clustering/ConsensusCluster.js';
-import { KNNClassifier, KNNRegressor } from './estimators/KNN.js';
-import { DecisionTreeClassifier, DecisionTreeRegressor } from './estimators/DecisionTree.js';
-import { RandomForestClassifier, RandomForestRegressor } from './estimators/RandomForest.js';
-import { GAMClassifier, GAMRegressor } from './estimators/GAM.js';
-import { PolynomialRegressor } from './estimators/PolynomialRegressor.js';
-import { MLPRegressor } from './estimators/MLPRegressor.js';
+import { KMeans } from "./estimators/KMeans.js";
+import { DBSCAN } from "./estimators/DBSCAN.js";
+import { HCA } from "./estimators/HCA.js";
+import { ConsensusCluster } from "../clustering/ConsensusCluster.js";
+import { KNNClassifier, KNNRegressor } from "./estimators/KNN.js";
+import {
+  DecisionTreeClassifier,
+  DecisionTreeRegressor,
+} from "./estimators/DecisionTree.js";
+import {
+  RandomForestClassifier,
+  RandomForestRegressor,
+} from "./estimators/RandomForest.js";
+import { GAMClassifier, GAMRegressor } from "./estimators/GAM.js";
+import { PolynomialRegressor } from "./estimators/PolynomialRegressor.js";
+import { MLPRegressor } from "./estimators/MLPRegressor.js";
 
 export {
   // Clustering algorithms
   DBSCAN,
   dbscan,
-  HDBSCAN,
-  hdbscan,
-  HDBSCANFast,
-  hdbscan_fast,
   ConsensusCluster,
   BranchPipeline,
   DecisionTreeClassifier,
